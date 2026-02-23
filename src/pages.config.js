da6e4e85efa2e8281797afa65e9f853f4d2ef47d
Magnus <haraldmagnus@gmail.com>
2026-02-23 16:17:46 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -47,50 +47,13 @@
  *
  * The mainPage value must match a key in the PAGES object exactly.
  */
-import AcceptCoLandlord from './pages/AcceptCoLandlord';
-import AddProperty from './pages/AddProperty';
-import CompleteProfile from './pages/CompleteProfile';
-import CreateAgreement from './pages/CreateAgreement';
-import EditProperty from './pages/EditProperty';
-import ErrorLogs from './pages/ErrorLogs';
-import FinancesExpense from './pages/FinancesExpense';
-import FinancesIncome from './pages/FinancesIncome';
-import FinancesNet from './pages/FinancesNet';
-import FinancesTax from './pages/FinancesTax';
-import Help from './pages/Help';
-import Invite from './pages/Invite';
-import Notifications from './pages/Notifications';
-import PaymentReminders from './pages/PaymentReminders';
-import RoleSelection from './pages/RoleSelection';
-import SignAgreement from './pages/SignAgreement';
-import TenantPhotos from './pages/TenantPhotos';
-import YearlyReport from './pages/YearlyReport';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "AcceptCoLandlord": AcceptCoLandlord,
-    "AddProperty": AddProperty,
-    "CompleteProfile": CompleteProfile,
-    "CreateAgreement": CreateAgreement,
-    "EditProperty": EditProperty,
-    "ErrorLogs": ErrorLogs,
-    "FinancesExpense": FinancesExpense,
-    "FinancesIncome": FinancesIncome,
-    "FinancesNet": FinancesNet,
-    "FinancesTax": FinancesTax,
-    "Help": Help,
-    "Invite": Invite,
-    "Notifications": Notifications,
-    "PaymentReminders": PaymentReminders,
-    "RoleSelection": RoleSelection,
-    "SignAgreement": SignAgreement,
-    "TenantPhotos": TenantPhotos,
-    "YearlyReport": YearlyReport,
 }
 
 export const pagesConfig = {
-    mainPage: "AcceptCoLandlord",
     Pages: PAGES,
     Layout: __Layout,
 };
